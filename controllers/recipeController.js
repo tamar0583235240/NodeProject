01d679dd117e8b53ad394b9174b0df9bb46a70c9
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-02-26 22:45:08 +0200

    תיאור של השינויים שביצעת

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,8 +1,18 @@
-exports.AddRecipe = (req, res) => {
-    
+const Recipe = require('../models/Recipe');
+exports.AddRecipe  =async (req, res) => {
+    const recipe = await Recipe.create(req.body);
+    res.json(recipe)
 }
 
-
+// exports.AllRecipe  =async (req, res) => {
+//     try {
+//         const recipe = await Recipe.find();
+//         res.json(recipe);
+//       } catch (error) {
+//         console.error('Failed to get users:', error);
+//         res.status(500).json({ message: 'Failed to get users' });
+//       }
+// }
 exports.DeleteRecipe = (req, res) => {
     
 }
