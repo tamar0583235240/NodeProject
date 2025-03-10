15120138f17af4152133e42d3022d58fc031d096
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-03-10 23:29:20 +0200

    UpdateRecipe

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -3,9 +3,11 @@ const express = require('express');
 const { AddRecipe } = require('../controllers/recipeController');
 const { DeleteRecipe } = require('../controllers/recipeController');
 const { UpdateRecipe } = require('../controllers/recipeController');
+const { AllRecipe } = require('../controllers/recipeController');
 
 const router = express.Router();
 
+router.post('/AllRecipe',AllRecipe)
 router.post('/AddRecipe',AddRecipe);
 router.delete('/DeleteRecipe',DeleteRecipe);
 router.put('/UpdateRecipe',UpdateRecipe);
