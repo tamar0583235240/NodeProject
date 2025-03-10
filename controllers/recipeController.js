c441d8e3f5b6f088af1f16e15697bd10609f3f30
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-03-10 20:53:34 +0200

    DeleteRecipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -13,8 +13,19 @@ exports.AllRecipe  =async (req, res) => {
         res.status(500).json({ message: 'Failed to get users' });
       }
 }
-exports.DeleteRecipe = (req, res) => {
-    
+exports.DeleteRecipe =async (req, res) => {
+  const  recipeName  = req.params.name;
+  console.log(recipeName+"bbbb");
+    try {
+      const deletedRecipe = await recipes.findOneAndDelete({ name: recipeName });
+      if (!deletedRecipe) {
+        return res.status(404).json({ message: 'recipe not found' });
+      }
+      res.json({ message: 'recipe deleted successfully' });
+    } catch (error) {
+      console.error('Failed to delete recipe:', error);
+      res.status(500).json({ message: 'Failed to delete recipe' });
+    }
 }
 
 exports.UpdateRecipe = (req, res) => {
